84e9b6acdd61829d5a4f3f9c2f4640bc6def7e96
Jake Munoz <jake.munoz@massive.co>
2020-10-03 13:37:48 +1000

    Updates

---
+++ precache-manifest.927700f3907898d54eda6a036670f881.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b3c51ffc78b9e04b27436aebb3f7c9",
+    "url": "/poncelet-fives/index.html"
+  },
+  {
+    "revision": "645703f555bd788d2a0b",
+    "url": "/poncelet-fives/static/js/2.9ab63ade.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/poncelet-fives/static/js/2.9ab63ade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe58f51ddd439494658",
+    "url": "/poncelet-fives/static/js/main.5e03224b.chunk.js"
+  },
+  {
+    "revision": "febe57e0971aa815d6a8",
+    "url": "/poncelet-fives/static/js/runtime-main.21555942.js"
+  }
+]);
